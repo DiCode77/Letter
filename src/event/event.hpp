ab39337d4f0186f6ba03499091c8cb4fba13c0f0
DiCode77 <109686137+DiCode77@users.noreply.github.com>
2026-09-03 00:14:38 +0300

    Added CMakefile project settings.

---
+++ src/event/event.hpp
@@ -0,0 +1,11 @@
+//
+//  event.hpp
+//  Letter
+//
+//  Created by DiCode77.
+//
+
+#ifndef event_hpp
+#define event_hpp
+
+#endif
---
+++ src/event/event_property.hpp
@@ -0,0 +1,11 @@
+//
+//  event_property.hpp
+//  Letter
+//
+//  Created by DiCode77.
+//
+
+#ifndef event_property_hpp
+#define event_property_hpp
+
+#endif
